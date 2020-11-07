5e58756e8f21ca3b4e7e8302a2fe8c0a456e1c73
Fred Buchanan <oscillator.b@gmail.com>
2020-11-07 15:17:20 -0500

    Added campus selection.

--- application/static/map.js
+++ application/static/map.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
+    // Configure the map
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/dark-v10',
         center: [DEFAULT_CENTER_LONG, DEFAULT_CENTER_LAT],
         zoom: 16
     });
+
+
+    // Setup auto-submit for campus form
+    $("#campus-select").change(function() {
+        console.log("change");
+        $("#campus-form").submit();
+    })
 });
--- application/views.py
+++ application/views.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import request, render_template
 from application import app
+from application.models import Campus
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    current_campus = Campus.query.filter_by(id=request.args.get('campus')).first_or_404()
+    campuses = Campus.query.all()
+
+    return render_template('index.html', current_campus=current_campus, campuses=campuses)
