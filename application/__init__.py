40ffbf08257ccaad7cdfa41d8c6bbe8af4ee6ac9
Fred Buchanan <oscillator.b@gmail.com>
2020-11-07 14:44:25 -0500

    Changed admin map theme.

--- application/__init__.py
+++ application/__init__.py
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # initialize admin
-app.config['MAPBOX_MAP_ID'] = 'bright-v9'
+app.config['MAPBOX_MAP_ID'] = 'streets-v11'
 app.config['MAPBOX_ACCESS_TOKEN'] = 'pk.eyJ1IjoiYm9zY2lsbGF0b3IiLCJhIjoiY2oya3F1bzQzMDBrbjMzczVoNjk5NzE4dCJ9.IGqUQQ3XNps5MVol_Raikg'
 app.config['DEFAULT_CENTER_LONG'] = -73.678172
 app.config['DEFAULT_CENTER_LAT'] = 42.729340
