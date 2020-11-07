9f4b5b6e365740f25bf982f8e3ed4639737ad978
Fred Buchanan <oscillator.b@gmail.com>
2020-11-07 15:49:15 -0500

    Added default campus if not specified.

--- application/views.py
+++ application/views.py
@@ -5,8 +5,11 @@ from application.models import Campus
 
 @app.route('/')
 def index():
+    if request.args.get('campus') is None:
+        current_campus = Campus.query.first()
+    else:
+        current_campus = Campus.query.filter_by(id=request.args.get('campus')).first_or_404()
 
-    current_campus = Campus.query.filter_by(id=request.args.get('campus')).first_or_404()
     campuses = Campus.query.all()
 
     return render_template('index.html', current_campus=current_campus, campuses=campuses)
